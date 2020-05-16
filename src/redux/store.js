53b93b463f238b8aedcb9e681a85dc5d68e828b9
devVinit <dev.vinit18@gmail.com>
2020-05-16 21:26:10 +0530

    retriving store from localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
 import { createStore } from 'redux';
 import rootReducers from '../redux/reducers/RootReducers';
+import { loadState, saveState } from '../localStorage';
 
-const store = createStore(rootReducers);
+const persistedState = loadState();
+
+const store = createStore(rootReducers, persistedState);
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
 
 export default store;
