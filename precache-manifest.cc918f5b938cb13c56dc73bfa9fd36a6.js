873cc51d93790cc3a525bbb08d61175309181978
devVinit <dev.vinit18@gmail.com>
2020-05-19 11:47:36 +0530

    Updates

---
+++ precache-manifest.cc918f5b938cb13c56dc73bfa9fd36a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08a566408204c09beda5d31dc38e2c2",
+    "url": "/inventory-app/index.html"
+  },
+  {
+    "revision": "40ed2e754c8c42736950",
+    "url": "/inventory-app/static/css/main.06770fb6.chunk.css"
+  },
+  {
+    "revision": "3f89632344d5842338fc",
+    "url": "/inventory-app/static/js/2.eea0c8f7.chunk.js"
+  },
+  {
+    "revision": "bdfdaee5779fe0c6a048c3619e9bf199",
+    "url": "/inventory-app/static/js/2.eea0c8f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ed2e754c8c42736950",
+    "url": "/inventory-app/static/js/main.bb025418.chunk.js"
+  },
+  {
+    "revision": "7780b68e34959936bfca",
+    "url": "/inventory-app/static/js/runtime-main.9f601d3e.js"
+  },
+  {
+    "revision": "fcc453b41702c3db31f2832278465e87",
+    "url": "/inventory-app/static/media/undraw_empty_xct9.fcc453b4.svg"
+  }
+]);
